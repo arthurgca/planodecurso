160fc2d5028c25d0b88f7df58199fb7dcf6a1d2f
Hilquias Abreu <hilquias@inventati.org>
2013-12-24 00:03:59 -0300

    Resolve import errors.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,26 +1,15 @@
 package controllers;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.data.Form;
-import play.data.validation.ValidationError;
 import play.libs.Json;
 
-import models.Periodo;
-import models.PlanoDeCurso;
 import models.CatalogoDeDisciplinas;
 import views.html.index;
 
 public class Application extends Controller {
 
     private static final CatalogoDeDisciplinas disciplinas = new CatalogoDeDisciplinas();
-
-    private static final Form<PlanoDeCurso> planoForm = Form.form(PlanoDeCurso.class);
 
     public static Result index() {
         return ok(index.render());
