bde03a9cba196c3fbb807d6c2f00a932c48ffd9e
Arthur Gurjao Coutinho Azevedo 21011048 <arthurgca@lcc2-09.lcc.ufcg.edu.br>
2014-02-14 15:14:13 -0300

    Importa disciplinas de arquivo.

--- app/models/CatalogoDeDisciplinas.java
+++ app/models/CatalogoDeDisciplinas.java
@@ -1,32 +1,63 @@
 package models;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 public class CatalogoDeDisciplinas {
 
-    private static final Map<String, Disciplina> disciplinas = new HashMap<String, Disciplina>();
+	private static final Map<Integer, Disciplina> disciplinas = new HashMap<Integer, Disciplina>();
 
-    public Disciplina get(String disciplinaId) {
-        if (!disciplinas.containsKey(disciplinaId)) {
-            throw new IllegalArgumentException();
-        }
-        
-        return disciplinas.get(disciplinaId);
-    }
+	public CatalogoDeDisciplinas(FileReader disciplinasXML) throws JDOMException, IOException {
+		importaDisciplinasXML(disciplinasXML);
+	}
 
-    public List<Disciplina> getAll() {
-        return new ArrayList<Disciplina>(disciplinas.values());
-    }
+	private void importaDisciplinasXML(FileReader disciplinasXML) throws JDOMException, IOException {
+		SAXBuilder sb = new SAXBuilder();
+		Document d = sb.build(disciplinasXML);
+		Element mural = d.getRootElement();
+		List<Element> elements = mural.getChildren();
+		Iterator<Element> i = elements.iterator();
+		while (i.hasNext()) {
+			Element element = (Element) i.next();
+			registraDisciplina(
+					Integer.parseInt(element.getAttributeValue("id")),
+					element.getAttributeValue("nome"),
+					Integer.parseInt(element.getChildText("creditos")),
+					Integer.parseInt(element.getChildText("periodo")),
+					Integer.parseInt(element.getChildText("dificuldade")),
+					element.getChildText("requisitos"));
+		}
+	}
 
-    public static void register(String id, String nome, int creditos, String... dependenciaIds) {
-        List<Disciplina> dependencias = new ArrayList<Disciplina>();
+	
+	private void registraDisciplina(int id, String nome, int creditos,
+			int periodo, int dificuldade, String... dependenciaIds) {
+		List<Disciplina> dependencias = new ArrayList<Disciplina>();
 
-        for (String dependenciaId : dependenciaIds) {
-            dependencias.add(disciplinas.get(dependenciaId));
-        }
+		for (String dependenciaId : dependenciaIds) {
+			dependencias.add(disciplinas.get(dependenciaId));
+		}
 
-        Disciplina[] dependenciasArray = dependencias.toArray(new Disciplina[dependencias.size()]);
-        disciplinas.put(id, new Disciplina(id, nome, creditos, dependenciasArray));
-    }
+		Disciplina[] dependenciasArray = dependencias
+				.toArray(new Disciplina[dependencias.size()]);
+		disciplinas.put(id, new Disciplina(id, nome, creditos, periodo,	dificuldade, dependenciasArray));
+	}
 
+	public Disciplina get(int i) {
+		if (!disciplinas.containsKey(i)) {
+			throw new IllegalArgumentException();
+		}
+
+		return disciplinas.get(i);
+	}
+
+	public Collection<Disciplina> getDisciplinas() {
+		return Collections.unmodifiableCollection(disciplinas.values());
+	}
 }
--- test/models/CatalogoDeDisciplinasTest.java
+++ test/models/CatalogoDeDisciplinasTest.java
@@ -1,48 +1,50 @@
 package models;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.jdom2.JDOMException;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CatalogoDeDisciplinasTest {
 
-    private CatalogoDeDisciplinas disciplinas;
+	private CatalogoDeDisciplinas catalogo;
 
-    @Before
-    public void setup() {
-        disciplinas = new CatalogoDeDisciplinas();
-    }
+	@Before
+	public void setUp() throws JDOMException, IOException {
+		FileReader disciplinasXML = new FileReader(
+				"test/support/disciplinas_testing.xml");
+		catalogo = new CatalogoDeDisciplinas(disciplinasXML);
+	}
 
-    @Test
-    public void deveRetornarDisciplinaPorId() {
-        assertThat(disciplinas.get("CALCULO2")).isInstanceOf(Disciplina.class);
-    }
+	@Test
+	public void deveImportarDisciplinasXML() {
+		assertEquals(catalogo.get(6), new Disciplina(6, "Lab. de Programação I", 4, 1, 4));
+		assertEquals(catalogo.get(7), new Disciplina(7, "Programação II", 4, 2, 8,
+				new Disciplina(1, "Programação I", 4, 1, 4),
+				new Disciplina(5, "Int. à Computacação", 4, 1, 5),
+				new Disciplina(6, "Lab. de Programação I", 4, 1, 4)));
+	}
 
-    @Test(expected=IllegalArgumentException.class)
-    public void deveLancarExcecaoQuandoIdDaDisciplinaNaoExiste() {
-        disciplinas.get("MINECRAFT");
-    }
+	@Test
+	public void deveRetornarDisciplinaPorId() {
+		assertThat(catalogo.get(1)).isInstanceOf(Disciplina.class);
+		assertThat(catalogo.get(7)).isInstanceOf(Disciplina.class);
+	}
 
-    @Test
-    public void deveConterDisciplinasDoPrimeiroPeriodo() {
-        assertThat(disciplinas.getAll()).onProperty("id").contains("CALCULO1", "LPT", "P1", "LP1");
-    }
-
-    @Test
-    public void deveConterDisciplinasDoSegundoPeriodo() {
-        assertThat(disciplinas.getAll()).onProperty("id").contains("CALCULO2", "DISCRETA", "P2");
-    }
-
-    @Test
-    public void deveConterDisciplinasDoTerceiroPeriodo() {
-        assertThat(disciplinas.getAll()).onProperty("id").contains("LINEAR", "PROBABILIDADE", "EDA");
-    }
-    
-    @Test
-    public void devePermitirCadastroDeDisciplinas() {
-    	CatalogoDeDisciplinas.register("MINECRAFT1", "Minecraft 1", 6, "SI1");
-    	disciplinas.get("MINECRAFT1");
-    }
-
+	@Test(expected = IllegalArgumentException.class)
+	public void deveLancarExcecaoQuandoIdDaDisciplinaNaoExiste() {
+		catalogo.get(9999);
+	}
+	
+	@Test(expected=UnsupportedOperationException.class)
+	public void deveRetornarDisciplinasComoListaImutavel() {
+		Collection<Disciplina> disciplinas = catalogo.getDisciplinas();
+		disciplinas.add(new Disciplina(2, "Teste", 4, 4, 4));
+	}
 }
