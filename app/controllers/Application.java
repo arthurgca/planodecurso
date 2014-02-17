2bfb589c1de7eedfae0dd426dfdb7a7d8b5e1523
Hilquias Abreu <hilquias@inventati.org>
2014-02-17 09:33:59 -0300

    Remove legado do Lab3

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,21 +2,13 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.libs.Json;
 
-import models.CatalogoDeDisciplinas;
 import views.html.index;
 
 public class Application extends Controller {
 
-    private static final CatalogoDeDisciplinas disciplinas = new CatalogoDeDisciplinas();
-
     public static Result index() {
         return ok(index.render());
-    }
-
-    public static Result disciplinasJson() {
-        return ok(Json.toJson(disciplinas.getAll()));
     }
 
 }
