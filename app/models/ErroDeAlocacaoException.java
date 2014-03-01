3963313babf07a34019f99566d53cec5ee48fc5f
Hilquias Abreu <hilquias@inventati.org>
2014-03-01 11:53:21 -0300

    Remove campo não usado.

--- app/models/ErroDeAlocacaoException.java
+++ app/models/ErroDeAlocacaoException.java
@@ -1,21 +1,7 @@
 package models;
 
 public class ErroDeAlocacaoException extends Exception {
-
-    private String msg;
-
     public ErroDeAlocacaoException(String msg) {
         super(msg);
-        this.msg = msg;
     }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-
 }
