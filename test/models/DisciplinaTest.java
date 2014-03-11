2ccd8c76ba1a2837fb891e906cdb4129a612fbd7
Hilquias Abreu <hilquias@inventati.org>
2014-03-11 12:59:29 -0300

    Remove asserções desnecessárias.

--- test/models/DisciplinaTest.java
+++ test/models/DisciplinaTest.java
@@ -13,22 +13,9 @@ public class DisciplinaTest extends test.TestBase {
     @Test
     public void serializaCorretamente() {
         JsonNode node = Json.toJson(disciplina("Programação II"));
-
-        assertEquals(7, node.get("id").intValue());
+        assertEquals(7, node.get("id").numberValue());
         assertEquals("Programação II", node.get("nome").textValue());
-        assertEquals(4, node.get("creditos").intValue());
-
-        Iterator<JsonNode> requisitoNodes = node.get("requisitos").elements();
-
-        Set<String> requisitos = new HashSet<String>();
-        requisitos.add(requisitoNodes.next().get("nome").textValue());
-        requisitos.add(requisitoNodes.next().get("nome").textValue());
-        requisitos.add(requisitoNodes.next().get("nome").textValue());
-
-        assertTrue(requisitos.contains("Programação I"));
-        assertTrue(requisitos.contains("Int. à Computação"));
-        assertTrue(requisitos.contains("Lab. de Programação I"));
-
-        assertFalse(requisitoNodes.hasNext());
+        assertEquals(4, node.get("creditos").numberValue());
+        assertTrue(node.get("requisitos").isArray());
     }
 }
