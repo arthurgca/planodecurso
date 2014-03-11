5cdba23486b6402cb7a7e580fbd1d1e7e04416eb
Hilquias Abreu <hilquias@inventati.org>
2014-03-11 12:09:43 -0300

    Remove entidade Alocacao.

--- app/models/PlanoDeCurso.java
+++ app/models/PlanoDeCurso.java
@@ -8,153 +8,41 @@ import com.avaje.ebean.*;
 
 @Entity
 public class PlanoDeCurso extends Model {
-    private static final int MAX_NUMERO_SEMESTRES = 14;
-    private static final int MAX_CREDITOS_SEMESTRE = 28;
 
     @Id
     public Long id;
-
-    @OneToMany(cascade = CascadeType.ALL)
-    public Set<Alocacao> alocacoes = new HashSet<Alocacao>();
 
     public PlanoDeCurso() {
     }
 
     public Set<Disciplina> getDisciplinas() {
         Set<Disciplina> disciplinas = new HashSet<Disciplina>();
-        for (Alocacao alocacao : alocacoes) {
-            disciplinas.add(alocacao.disciplina);
-        }
         return disciplinas;
     }
 
    public Set<Disciplina> getDisciplinas(int semestre) {
        Set<Disciplina> disciplinas = new HashSet<Disciplina>();
-       for (Alocacao alocacao : alocacoes) {
-           if (alocacao.semestre == semestre) {
-               disciplinas.add(alocacao.disciplina);
-           }
-       }
        return disciplinas;
     }
 
     public int getTotalCreditos(int semestre) {
         int totalCreditos = 0;
-        for (Disciplina disciplina : getDisciplinas(semestre)) {
-            totalCreditos += disciplina.creditos;
-        }
         return totalCreditos;
     }
 
     public void alocarDisciplina(int semestre, Disciplina disciplina) throws ErroDeAlocacaoException {
-        validaAlocacao(semestre, disciplina, false);
-        alocacoes.add(new Alocacao(semestre, disciplina));
         save();
     }
 
     public void moverDisciplina(int semestre, Disciplina disciplina) throws ErroDeAlocacaoException {
-        Alocacao alocacao = getAlocacao(disciplina);
-        if (alocacao == null) {
-            return;
-        }
-
-        validaAlocacao(semestre, disciplina, true);
-        alocacao.semestre = semestre;
         save();
     }
 
     public void desalocarDisciplina(Disciplina disciplina) {
-        Alocacao alocacao = getAlocacao(disciplina);
-        if (alocacao != null) {
-            desalocarDisciplinaRecursivamente(alocacao);
-            save();
-        }
     }
 
-    private void desalocarDisciplinaRecursivamente(Alocacao alocacao) {
-        alocacoes.remove(alocacao);
+    private void desalocarDisciplinaRecursivamente(int semestre, Disciplina disciplina) {
 
-        List<Alocacao> filaRemocao = new ArrayList<Alocacao>();
-
-        for (Alocacao i : alocacoes) {
-            if (i.disciplina.requisitos.contains(alocacao.disciplina)) {
-                filaRemocao.add(i);
-            }
-        }
-
-        for (Alocacao i : filaRemocao) {
-            desalocarDisciplinaRecursivamente(i);
-        }
-    }
-
-    private Alocacao getAlocacao(Disciplina disciplina) {
-        for (Alocacao alocacao : alocacoes) {
-            if (alocacao.disciplina.equals(disciplina)) {
-                return alocacao;
-            }
-        }
-        return null;
-    }
-
-    private void validaAlocacao(
-          int semestre,
-          Disciplina disciplina,
-          boolean isMovimento) throws ErroDeAlocacaoException {
-
-        String template;
-        String message;
-
-        if (semestre < 1 || semestre > MAX_NUMERO_SEMESTRES) {
-            template = "O período precisa estar entre %s e %s.";
-            message = String.format(template, 1, MAX_NUMERO_SEMESTRES);
-            throw new ErroDeAlocacaoException(message);
-        }
-
-        int totalCreditos = getTotalCreditos(semestre) + disciplina.creditos;
-
-        if (totalCreditos > MAX_CREDITOS_SEMESTRE) {
-            template = "<b>%s</b> ultrapassa o limite do <b>%sº período.</b>";
-            message = String.format(template, disciplina.nome, semestre);
-            throw new ErroDeAlocacaoException(message);
-        }
-
-        if (isMovimento) {
-            return;
-        }
-
-        Alocacao alocacaoAnterior = getAlocacao(disciplina);
-
-        if (alocacaoAnterior != null) {
-            template = "<b>%s</b> já foi alocada no <b>%sº período.</b>";
-            message = String.format(template, disciplina.nome, semestre);
-            throw new ErroDeAlocacaoException(message);
-        }
-
-        Set<Disciplina> pagas = new HashSet<Disciplina>();
-
-        for (int i = 1; i < semestre; i++) {
-            pagas.addAll(getDisciplinas(i));
-        }
-
-        Set<Disciplina> devendo = new HashSet<Disciplina>(disciplina.requisitos);
-        devendo.removeAll(pagas);
-
-        if (!devendo.isEmpty()) {
-            template = "<b>%s</b> tem <b>requisitos não satisfeitos</b>: %s.";
-
-            StringBuilder sb = new StringBuilder();
-            Iterator<Disciplina> it = devendo.iterator();
-            while (it.hasNext()) {
-                Disciplina requisito = it.next();
-                sb.append(requisito.nome);
-                if (it.hasNext())
-                    sb.append(", ");
-            }
-            String joined = sb.toString();
-
-            message = String.format(template, disciplina.nome, joined);
-            throw new ErroDeAlocacaoException(message);
-        }
     }
 
     public static PlanoDeCurso criarPlanoInicial() {
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,14 +3,6 @@
 
 # --- !Ups
 
-create table alocacao (
-  id                        bigint not null,
-  plano_de_curso_id         bigint not null,
-  semestre                  integer,
-  disciplina_id             integer,
-  constraint pk_alocacao primary key (id))
-;
-
 create table disciplina (
   id                        integer not null,
   nome                      varchar(255),
@@ -29,16 +21,10 @@ create table disciplina_requisitos (
   requisito_id                   integer not null,
   constraint pk_disciplina_requisitos primary key (disciplina_id, requisito_id))
 ;
-create sequence alocacao_seq;
-
 create sequence disciplina_seq;
 
 create sequence plano_de_curso_seq;
 
-alter table alocacao add constraint fk_alocacao_plano_de_curso_1 foreign key (plano_de_curso_id) references plano_de_curso (id) on delete restrict on update restrict;
-create index ix_alocacao_plano_de_curso_1 on alocacao (plano_de_curso_id);
-alter table alocacao add constraint fk_alocacao_disciplina_2 foreign key (disciplina_id) references disciplina (id) on delete restrict on update restrict;
-create index ix_alocacao_disciplina_2 on alocacao (disciplina_id);
 
 
 
@@ -50,8 +36,6 @@ alter table disciplina_requisitos add constraint fk_disciplina_requisitos_disc_0
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists alocacao;
-
 drop table if exists disciplina;
 
 drop table if exists disciplina_requisitos;
@@ -59,8 +43,6 @@ drop table if exists disciplina_requisitos;
 drop table if exists plano_de_curso;
 
 SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists alocacao_seq;
 
 drop sequence if exists disciplina_seq;
 
